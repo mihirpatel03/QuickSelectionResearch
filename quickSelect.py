3fc6a4c2ad1c29a4283e86aab253cab6753cfcd6
Mihir Patel <mihirdpatel@live.com>
2023-07-06 15:39:10 -0400

    simul. data collection

--- quickSelect.py
+++ quickSelect.py
@@ -1,7 +1,7 @@
 import random
 
 
-def qs(arr, k, l, r):
+def qs(arr, k, l, r, pc):
     # given an array, a value k, and left and right indices, will return the kth smallest value
     # in that array between those two indices
 
@@ -9,9 +9,9 @@ def qs(arr, k, l, r):
     if r == l:
         return arr[l]
     # partition the array around the randomly chose pivot
-    pivotChoice = int(random.randint(l, r))
 
-    p = partition(arr, pivotChoice, l, r)
+    p = eval(pc)(arr, k, l, r)
+
     # length of the (sub)array from left to pivot after partition
     left_length = p-l
 
@@ -23,15 +23,15 @@ def qs(arr, k, l, r):
     # CASE 2: recur on the left side. e.g., if there are 3 elements to the left of the pivot and we are
     # looking for the second smallest element, we know it must be in that subarray to the left of the pivot
     elif left_length+1 > k:
-        print("now recurring on a subarray of length " + str(p-1-l))
-        return qs(arr, k, l, p-1)
+        # print("now recurring on a subarray of length " + str(p-1-l))
+        return qs(arr, k, l, p-1, pc)
 
     # CASE 3: recur on the right side. e.g., if we were looking for the 5th smallest element in an array
     # of size 8, and the pivot was at index 3 (3 elements to its left), we are now looking for the 1st
     # smallest element in the right subarray, of length 4.
     else:
-        print("now recurring on a subarray of length " + str(r-(p+1)))
-        return qs(arr, k-left_length-1, p+1, r)
+        # print("now recurring on a subarray of length " + str(r-(p+1)))
+        return qs(arr, k-left_length-1, p+1, r, pc)
 
 
 def partition(arr, pivot_idx, l, r):
@@ -61,22 +61,25 @@ def partition(arr, pivot_idx, l, r):
     return idx
 
 
-def default(l, r):
-    return int(random.randint(l, r))
+def default(arr, k, l, r):
+    pivotChoice = int(random.randint(l, r))
+    return partition(arr, pivotChoice, l, r)
 
 
-def probabilistic():
-    # pivotChoice = int(random.randint(l, r))
-    # p = partition(arr, pivotChoice, l, r)
+def probabilistic(arr, k, l, r):
+    lowBound = 1/4.0
+    highBound = 3/4.0
 
-    # lowBound = 1/4.0
-    # highBound = 3/4.0
+    pivotChoice = int(random.randint(l, r))
 
-    # # need to randomize pivot selection
-    # while p < int(lowBound*(r-l))+l or p > int(highBound*(r-l))+l:
-    # pivotChoice = int(random.randint(l, r))
-    # p = partition(arr, pivotChoice, l, r)
-    return
+    p = partition(arr, pivotChoice, l, r)
+
+    # need to randomize pivot selection
+    while p < int(lowBound*(r-l))+l or p > int(highBound*(r-l))+l:
+        pivotChoice = int(random.randint(l, r))
+        p = partition(arr, pivotChoice, l, r)
+
+    return p
 
 
 def deterministic():
