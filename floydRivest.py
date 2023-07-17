72ec574a796c531f251cb1b4e6e653a84bd2b4d0
Mihir Patel <mihirdpatel@live.com>
2023-07-17 18:22:34 -0400

    fixed FRA, started testing w/ time+ops

--- floydRivest.py
+++ floydRivest.py
@@ -1,62 +1,61 @@
 import math
 import random
 
-# Function to return the
-# sign of the number
 
+class FloydRivest:
+    def __init__(self, input_array):
+        self.partitionIterations = 0
+        self.arr = input_array
 
-def sign(x):
-    if x > 0:
-        return 1
-    elif x < 0:
-        return -1
-    return 0
+    def sign(self, x):
+        if x > 0:
+            return 1
+        elif x < 0:
+            return -1
+        return 0
 
-# Function to return kth smallest number
+    def select(self, k: int, left: int,
+               right: int):
+        while right > left:
 
-
-def select(arr: list, left: int,
-           right: int, k: int):
-    while right > left:
-
-        # Choosing a small subarray
-        # S based on sampling.
-        # 600, 0.5 and 0.5 are
-        # arbitrary constants
-        if right-left > 600:
-            n = right - left + 1
-            i = k - left + 1
-            z = math.log(n)
-            s = 0.5 * math.exp(2 * z / 3)
-            sd = 0.5 * math.sqrt(z * s * (n-s)/n) * sign(i-n / 2)
-            newLeft = int(max(left, k-i * s / n + sd))
-            newRight = int(min(right, k + (n - i) * s / n + sd))
-            select(arr, newLeft, newRight, k)
-        t = arr[k]
-        i = left
-        j = right
-        arr[left], arr[k] = arr[k], arr[left]
-        if arr[right] > t:
-            arr[left], arr[right] = arr[right], arr[left]
-        while i < j:
-            arr[i], arr[j] = arr[j], arr[i]
-            i = i + 1
-            j = j-1
-            while arr[i] < t:
+            # Choosing a small subarray
+            # S based on sampling.
+            # 600, 0.5 and 0.5 are
+            # arbitrary constants
+            if right-left > 600:
+                n = right - left + 1
+                i = k - left + 1
+                z = math.log(n)
+                s = 0.5 * math.exp(2 * z / 3)
+                sd = 0.5 * math.sqrt(z * s * (n-s)/n) * self.sign(i-n / 2)
+                newLeft = int(max(left, k-i * s / n + sd))
+                newRight = int(min(right, k + (n - i) * s / n + sd))
+                self.select(k, newLeft, newRight)
+            t = self.arr[k]
+            i = left
+            j = right
+            self.arr[left], self.arr[k] = self.arr[k], self.arr[left]
+            if self.arr[right] > t:
+                self.arr[left], self.arr[right] = self.arr[right], self.arr[left]
+            while i < j:
+                self.arr[i], self.arr[j] = self.arr[j], self.arr[i]
                 i = i + 1
-            while arr[j] > t:
                 j = j-1
+                while self.arr[i] < t:
+                    i = i + 1
+                while self.arr[j] > t:
+                    j = j-1
 
-        if arr[left] == t:
-            arr[left], arr[j] = arr[j], arr[left]
-        else:
-            j = j + 1
-            arr[right], arr[j] = arr[j], arr[right]
+            if self.arr[left] == t:
+                self.arr[left], self.arr[j] = self.arr[j], self.arr[left]
+            else:
+                j = j + 1
+                self.arr[right], self.arr[j] = self.arr[j], self.arr[right]
 
-        # Updating the left and right indices
-        # depending on position of k-th element
-        if j <= k:
-            left = j + 1
-        if k <= j:
-            right = j-1
-    return arr[k]
+            # Updating the left and right indices
+            # depending on position of k-th element
+            if j <= k:
+                left = j + 1
+            if k <= j:
+                right = j-1
+        return self.arr[k]
--- timeCollection.py
+++ timeCollection.py
@@ -3,11 +3,11 @@ import random
 import time
 from default import Default
 from dynamic import Dynamic
-import floydRivest
+from floydRivest import FloydRivest
 
 # if you want to check quickSelect's correctness by sorting the list and accessing its kth element, set this to true.
 # Works for smaller values but too time consuming for larger values, so commented out when calculating the runtimes
-verifyResults = True
+verifyResults = False
 
 
 def createTestArray(maxVal, numItems):
@@ -24,7 +24,7 @@ def avg(input_list):
 
 if __name__ == "__main__":
 
-    max_value, array_size, numRuns = 100000000, 1000000, 5
+    max_value, array_size, numRuns = 100000000, 500000, 5
     defaultTimes, dynamicTimes, fraTimes = [], [], []
     defaultOps, dynamicOps, fraOps = [], [], []
     for i in range(numRuns):
@@ -37,25 +37,32 @@ if __name__ == "__main__":
         defaultTimes.append(time.time()-start)
         defaultOps.append(default.partitionIterations)
 
-        dynamic = Dynamic(inp)
+        dynamic = Dynamic(inp[:])
         start = time.time()
         dynamicResult = dynamic.qs(kth, 0, len(inp)-1)
         dynamicTimes.append(time.time()-start)
         dynamicOps.append(dynamic.partitionIterations)
 
-        # start = time.time()
-        # fraResult = floydRivest.select(fra_array, kth, 0, len(fra_array)-1)
-        # fraTimes.append(time.time()-start)
+        floydRivest = FloydRivest(inp)
+        start = time.time()
+        fraResult = floydRivest.select(kth-1, 0, len(inp)-1)
+        fraTimes.append(time.time()-start)
 
         if (verifyResults):
             sorted_array = sorted(inp)
             correctVal = sorted_array[kth-1]
+            assert correctVal == defaultResult == dynamicResult == fraResult
 
-            assert correctVal == defaultResult == dynamicResult
-            # print(correctVal, defaultResult, dynamicResult, fraResult)
-
-    print(avg(defaultTimes), defaultTimes)
-    print(avg(dynamicTimes), dynamicTimes)
-    # print(avg(fraTimes), fraTimes)
-    print(avg(defaultOps), defaultOps)
-    print(avg(dynamicOps), dynamicOps)
+    print("for list size " + str(array_size) + "...")
+    print("\n TIMES \n")
+    print("Default takes on average " + str(avg(defaultTimes)) +
+          ", here are each of the times: ", defaultTimes)
+    print("Dynamic takes on average " + str(avg(dynamicTimes)) +
+          ", here are each of the times: ", dynamicTimes)
+    print("Floyd-Rivest takes on average " + str(avg(fraTimes)) +
+          ", here are each of the times: ", fraTimes)
+    print("\n OPERATIONS \n")
+    print("Default takes on average " + str(avg(defaultOps)) +
+          ", here are each of the operations: ", defaultOps)
+    print("Dynamic takes on average " + str(avg(dynamicOps)) +
+          ", here are each of the operations: ", dynamicOps)
